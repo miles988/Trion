1bb0398b3e778513b7b4594053bcad0ee99c2583
miles988 <odiamuslonon@gmail.com>
2024-08-09 22:56:51 +0100

    Update ai3.js

--- scripts/cmds/ai3.js
+++ scripts/cmds/ai3.js
@@ -1,1 +1,54 @@
+const axios = require('axios');
 
+const Prefixes = [
+  'ai',
+  'anya',
+  'perfect',
+  '+ai',
+  'hi',
+  '.ai',
+  'ask',
+];
+
+module.exports = {
+  config: {
+    name: "ask",
+    version: 1.0,
+    author: "OtinXSandip",
+    longDescription: "AI",
+    category: "ai",
+    guide: {
+      en: "{p} questions",
+    },
+  },
+  onStart: async function () {},
+  onChat: async function ({ api, event, args, message }) {
+    try {
+
+      const prefix = Prefixes.find((p) => event.body && event.body.toLowerCase().startsWith(p));
+      if (!prefix) {
+        return; // Invalid prefix, ignore the command
+      }
+      const prompt = event.body.substring(prefix.length).trim();
+   if (!prompt) {
+        await message.reply("𝑨𝒏𝒚𝒂 𝒊𝒔 𝒉𝒆𝒓𝒆 𝒘𝒊𝒍𝒍 𝒚𝒐𝒖 𝒑𝒓𝒐𝒗𝒊𝒅𝒆 𝒎𝒆 𝒕𝒉𝒆 𝒒𝒖𝒆𝒔𝒕𝒊𝒐𝒏 𝒕𝒐 𝒔𝒐𝒍𝒗𝒆 𝒊𝒕 (•̀ᴗ•́)و");
+        return;
+      }
+
+
+      const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+      const answer = response.data.answer;
+
+
+    await message.reply({ body: `𝓜𝓻 𝓞𝓭𝓲𝓪𝓶𝓾𝓼 𝓐𝓲
+______________________________  
+${answer}
+𝑩𝒐𝒕 𝒐𝒘𝒏𝒆𝒓 
+m.me/100080855610572`,
+});
+
+   } catch (error) {
+      console.error("Error:", error.message);
+    }
+  }
+};
